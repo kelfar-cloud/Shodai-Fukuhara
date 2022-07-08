d0da5522dc8e77e6a3fba30e5153cd0e2b840331
kelfar <macchan@MacchannoMacBook-Pro.local>
2022-07-08 13:54:22 +0900

    PC版完成

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("scroll-nav", window.scrollY > 500);
 })
+
+const button = document.querySelector("#contact_button");
+button.addEventListener("click", () => {
+  window.location.href = "service.html";
+});
